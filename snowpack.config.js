25d31a5934cb51d719159f1bc4149929e64bcf86
Pablo Berganza <pablo@berganza.dev>
2021-01-15 14:04:44 +0000

    :bug: fix deploy

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,8 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: {url: process.env.CI ? '/snowpack-react-swapi' : '/', static: true},
+    src: {url: process.env.CI ? '/snowpack-react-swapi/dist' : '/dist'},
   },
   plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
   routes: [
